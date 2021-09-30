b488cef0f501b81eba1102f1c8d4c998faee02b6
Marco Antônio Chaves Soares <macsoares1@gmail.com>
2021-09-30 09:54:21 -0300

    Update ThemeContext.ts

--- lib/contexts/ThemeContext.ts
+++ lib/contexts/ThemeContext.ts
@@ -1,7 +1,7 @@
-import { lightTheme } from "lib/theme/theme";
+import themeBuilder from "lib/theme/theme";
 import { createContext } from "react";
 
 export default createContext({
   toggleTheme: () => {},
-  theme: lightTheme
+  theme: themeBuilder()
 })
